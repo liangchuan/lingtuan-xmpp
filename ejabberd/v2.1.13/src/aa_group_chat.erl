390c0e30a0bbe9683d8c139ea4e3fd6620450977
cc14514 <cc14514@github.com>
2014-03-17 17:26:33 +0800

    fix group msg route

--- ejabberd/v2.1.13/src/aa_group_chat.erl
+++ ejabberd/v2.1.13/src/aa_group_chat.erl
@@ -105,20 +105,25 @@ get_user_list_by_group_id(Domain,GroupId)->
 			{error,Reason}
      	end.
 
-route_msg(From,#jid{user=User,server=Domain}=To,Packet,GroupId) ->
-	{X,E,Attr,Body} = Packet,
-	?DEBUG("##### route_group_msg_003 param :::> {User,Domain,GroupId}=~p",[{User,Domain,GroupId}]),
-	RAttr0 = lists:map(fun({K,V})-> case K=:="to" of true-> {K,User++"@"++Domain}; _-> {K,V} end end,Attr),
-	RAttr1 = lists:append(RAttr0,[{"groupid",GroupId}]),
-	RPacket = {X,E,RAttr1,Body},
-	?DEBUG("###### route_group_msg 003 input :::> {From,To,RPacket}=~p",[{From,To,RPacket}]),
-	case ejabberd_router:route(From, To, RPacket) of
-		ok ->
-			?DEBUG("###### route_group_msg 003 OK :::> {From,To,RPacket}=~p",[{From,To,RPacket}]),
-			{ok,ok};
-		Err ->
-			?DEBUG("###### route_group_msg 003 ERR=~p :::> {From,To,RPacket}=~p",[Err,{From,To,RPacket}]),
-			{error,Err}
+route_msg(#jid{user=FromUser}=From,#jid{user=User,server=Domain}=To,Packet,GroupId) ->
+	case FromUser=/=User of
+		true->
+			{X,E,Attr,Body} = Packet,
+			?DEBUG("##### route_group_msg_003 param :::> {User,Domain,GroupId}=~p",[{User,Domain,GroupId}]),
+			RAttr0 = lists:map(fun({K,V})-> case K of "to" -> {K,User++"@"++Domain}; "msgtype" -> {K,"groupchat"};	_-> {K,V} end end,Attr),
+			RAttr1 = lists:append(RAttr0,[{"groupid",GroupId}]),
+			RPacket = {X,E,RAttr1,Body},
+			?DEBUG("###### route_group_msg 003 input :::> {From,To,RPacket}=~p",[{From,To,RPacket}]),
+			case ejabberd_router:route(From, To, RPacket) of
+				ok ->
+					?DEBUG("###### route_group_msg 003 OK :::> {From,To,RPacket}=~p",[{From,To,RPacket}]),
+					{ok,ok};
+				Err ->
+					?DEBUG("###### route_group_msg 003 ERR=~p :::> {From,To,RPacket}=~p",[Err,{From,To,RPacket}]),
+					{error,Err}
+			end;
+		_ ->
+			{ok,skip}
 	end.
 
 is_group_chat(#jid{server=Domain}=To)->
