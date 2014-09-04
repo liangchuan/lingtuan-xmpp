ce0231ea30e9355755d850b872eecdf7220ac665
cc14514 <cc14514@icloud.com>
2014-09-04 18:35:25 +0800

    fix

--- ejabberd/v2.1.13/src/aa_http.erl
+++ ejabberd/v2.1.13/src/aa_http.erl
@@ -40,8 +40,8 @@ handle_http(Req) ->
 
 http_response({S,Req}) ->
 	try
-		{M,S,SS} = now(),
-		SN_T = erlang:integer_to_list(M*1000000000000+S*1000000+SS),
+		{M,S1,SS1} = now(),
+		SN_T = erlang:integer_to_list(M*1000000000000+S1*1000000+SS1),
 		Res = {obj,[{sn,SN_T},{success,S#success.success},{entity,S#success.entity}]},
 		?DEBUG("Res_obj=~p",[Res]),
 		J = rfc4627:encode(Res),
