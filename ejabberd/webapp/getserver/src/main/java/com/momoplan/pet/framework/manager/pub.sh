f3c124bbae09ced3b4fdc48a38f1cbf2dec36e9a
cc14514 <cc14514@icloud.com>
2014-11-20 10:08:13 +0800

    script

---
+++ ejabberd/webapp/getserver/src/main/java/com/momoplan/pet/framework/manager/pub.sh
@@ -0,0 +1,32 @@
+#test
+scp -r service root@203.195.201.198:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+scp -r node root@203.195.201.198:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+scp -r controller root@203.195.201.198:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+
+# #s1
+# scp -r service root@203.195.205.87:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r node root@203.195.205.87:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r controller root@203.195.205.87:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# 
+# #s2
+# echo s2 = gxzk6aZ9hxweqDZ
+# scp -r service root@203.195.187.164:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r node root@203.195.187.164:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r controller root@203.195.187.164:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# 
+# #s3
+# echo s3 = NbAYArRL2udKL
+# scp -r service root@203.195.160.104:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r node root@203.195.160.104:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r controller root@203.195.160.104:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# 
+# #s4
+# echo s4 = NbAYArRL2udKL
+# scp -r service root@203.195.160.105:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r node root@203.195.160.105:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+# scp -r controller root@203.195.160.105:/data/service/webserver/8080/webapps/getserver/WEB-INF/classes/com/momoplan/pet/framework/manager/
+
+
+
+
+
